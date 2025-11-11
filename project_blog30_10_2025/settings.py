38e03a22eebfd004aaa3a0bbb12669ba8078d7c1
root <root@localhost.localdomain>
2025-11-11 08:35:48 +0000

    Fixing media

--- project_blog30_10_2025/settings.py
+++ project_blog30_10_2025/settings.py
@@ -147,7 +147,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+#MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
